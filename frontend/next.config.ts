7cd416144868131ad5cd22861042368cad35de77
highrack <nutgamon2547@gmail.com>
2026-07-02 23:52:03 +0700

    3nd commit 2769 fixThemeTypePath

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/chat',
+        permanent: true, // ตั้งเป็น true เพื่อให้บอทค้นหา (SEO) รู้ว่าย้ายหน้าถาวร
+      },
+    ]
+  },
 };
 
 export default nextConfig;
